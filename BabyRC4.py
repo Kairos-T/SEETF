ee858ae18cc92dfa1f3b645fdc0f0fe845c65965
Kairos-T <80029462+Kairos-T@users.noreply.github.com>
2023-06-26 14:24:24 +0800

    Update BabyRC4.py

--- BabyRC4.py
+++ BabyRC4.py
@@ -16,10 +16,9 @@ from os import urandom
 c0 = bytes.fromhex('b99665ef4329b168cc1d672dd51081b719e640286e1b0fb124403cb59ddb3cc74bda4fd85dfc')
 c1 = bytes.fromhex('a5c237b6102db668ce467579c702d5af4bec7e7d4c0831e3707438a6a3c818d019d555fc')
 
-# XOR c0 and c1 to obtain the keystream
+# XOR c0 and c1 
 keystream = bytes(c0_byte ^ c1_byte for c0_byte, c1_byte in zip(c0, c1))
 
-# XOR the keystream with a sequence of 'a's to recover the key
 key = bytes(keystream_byte ^ ord('a') for keystream_byte in keystream)
 
 print("Recovered key:", key.hex())
